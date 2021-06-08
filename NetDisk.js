b173fb43adbb2c872c4db8df49293c379794efd9
李光浩 <liguanghao@viatomtech.com>
2021-06-08 18:06:28 +0800

    df

--- NetDisk.js
+++ NetDisk.js
@@ -10,6 +10,33 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const multiparty = require('multiparty')
+ 
+const fs = require('fs')
+ 
+app.post("/info",  function (req, res) {
+  /* 生成multiparty对象，并配置上传目标路径 */
+  let form = new multiparty.Form();
+  // 设置编码
+  form.encoding = 'utf-8';
+  // 设置文件存储路径，以当前编辑的文件为相对路径
+  form.uploadDir = './images';
+  // 设置文件大小限制
+  // form.maxFilesSize = 1 * 1024 * 1024;
+  form.parse(req, function (err, fields, files) {
+    try {
+      let inputFile = files.file[0];
+      let newPath = form.uploadDir + "/" + inputFile.originalFilename;
+      // 同步重命名文件名 fs.renameSync(oldPath, newPath)
+　　　 //oldPath  不得作更改，使用默认上传路径就好
+      fs.renameSync(inputFile.path, newPath);
+      res.send({ data: "上传成功！" });
+    } catch (err) {
+      console.log(err);
+      res.send({ err: "上传失败！" });
+    };
+  })
+});
 
 
 app.post('/login',function(req,res){  
